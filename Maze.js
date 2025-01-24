b8f5f1c3b67c69bb9b37021ebdd7476693594b01
Wheels_Aiden <xaiden91@gmail.com>
2025-01-24 07:59:27 -0500

    Intial js program

---
+++ Maze.js
@@ -0,0 +1,62 @@
+//Team Members: Caleb Hermann & Aiden Walker
+//Date 1.24.2025
+//Program: Maze
+//Hardware: Bolt #SB-EC76
+
+//Begin Pragram
+async function startProgram() {
+
+  //Start & Say Start
+  //Bolt will say start across LED's & say it outloud
+  //Driver: Aiden
+  //Navigator: Caleb
+
+
+
+
+  // Green Checkpoint
+  //LED's turn Green
+  //Driver: Caleb
+  //Navigator: Aiden
+
+
+  // shound #1 Checkpoint
+  //Bolt makes sound of choice - Update Sound
+  //Driver: Caleb
+  //Navigator: Aiden
+
+
+  // Red Checkpoint
+  //LED's turn Red
+  //Driver: Aiden
+  //Navigator: Caleb
+
+
+  // shound #2 Checkpoint
+  //Bolt makes sound of choice - Update Sound
+  //Driver: Caleb
+  //Navigator: Aiden
+
+  // Purple Checkpoint
+  //LED's turn Purple
+  //Driver: Aiden
+  //Navigator: Caleb
+
+  // shound #3 Checkpoint
+  //Bolt makes sound of choice - Update Sound
+  //Driver: Caleb
+  //Navigator: Aiden
+
+  //Blue Checkpoint
+  //LED's turn Purple
+  //Driver: Aiden
+  //Navigator: Caleb
+
+
+  //Finished & Say Finish
+  //Bolt will say Finished across LED's & say it out loud
+  //Driver: Caleb
+  //Navigator: Aiden
+
+//End Program
+}
