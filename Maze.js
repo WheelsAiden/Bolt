1a57abb602385ef479d16dc922450f0033161dfe
Wheels_Aiden <xaiden91@gmail.com>
2025-02-04 08:10:17 -0500

    Sound #3 Checkpoint

--- Maze.js
+++ Maze.js
@@ -23,8 +23,9 @@
 //Navigator: Aiden
 
  await roll(0,74,2);
- await delay(2);
+ await delay(1.5);
  await setMainLed({r:0, g:255, b:0})
+
 
 //Sound #1 Checkpoint
 //Bolt makes sound of choice - Update Sound
@@ -32,10 +33,11 @@
 //Navigator: Caleb
 
  await roll(90, 53, 2);
- await delay (2);
- await roll (180, 38, 2);
- await delay (2);
+ await delay (1.5);
+ await roll (180, 40, 2);
+ await delay (1.5);
  await Sound.Animal.Monkey.play(true);
+
 
 //Red Checkpoint
 //LED's turn Red
@@ -43,10 +45,11 @@
 //Navigator: Aiden
 
  await roll(215, 41, 2);
- await delay (2);
- await roll(125, 31, 2);
- await delay (2);
+ await delay (1.5);
+ await roll(125, 28, 2);
+ await delay (1.5);
  await setMainLed({r:255, g:0, b:0})
+
 
 //Sound #2 Checkpoint
 //Bolt makes sound of choice - Update Sound
@@ -54,19 +57,19 @@
 //Navigator: Caleb
 
  await roll(35, 41, 2);
- await delay(2);
- await Sound.Animal.Monkey.Play(true);
+ await delay(1.5);
+ await Sound.Animal.Monkey.play(true);
 
 //Purple Checkpoint
 //LED's turn Purple
 //Driver: Caleb
 //Navigator: Aiden
 
-	await roll(125, 36, 2);
-	await delay (1.5);
-	await roll(180, 25, 2);
-	await setMainLed({r:255, g:0, b:255})
-	await delay (1.5);
+ await roll(125, 36, 2);
+ await delay (1.5);
+ await roll(180, 25, 2);
+ await setMainLed({r:255, g:0, b:255})
+ await delay (1.5);
 
 
 //Sound #3 Checkpoint
@@ -74,9 +77,14 @@
 //Driver: Aiden
 //Navigator: Caleb
 
+  await roll (90, 32, 2);
+  await delay (1.5);
+  await roll(0, 31, 2)
+  await delay(1.5)
+  await Sound.Animal.Monkey.play(true);
 
 //Blue Checkpoint
-//LED's turn Purple
+//LED's turn Blue
 //Driver: Caleb
 //Navigator: Aiden
 
